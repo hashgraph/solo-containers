3f5f44c408d592e187bf6759d8ca05e49b22ede2
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-03-17 09:31:19 -0500

    fix format
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- docker/ubi8-init-java21/stage_files.sh
+++ docker/ubi8-init-java21/stage_files.sh
@@ -20,7 +20,7 @@ set -eo pipefail
 files=$(ls -1 /etc/network-node/config)
 for file in $files; do
   if [ -f "/opt/hgcapp/services-hedera/HapiApp2.0/${file}" ]; then
-  rm -f "/opt/hgcapp/services-hedera/HapiApp2.0/${file}"
+    rm -f "/opt/hgcapp/services-hedera/HapiApp2.0/${file}"
   fi
   ln -s "/etc/network-node/config/${file}" "/opt/hgcapp/services-hedera/HapiApp2.0/${file}"
 done
