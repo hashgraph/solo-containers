2f7f07e6c6fb0be9e7b866458b13f79d6ce61b0c
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-03-26 14:31:23 -0500

    feat: clone backup loader from infrastructure container
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

---
+++ docker/backup-uploader/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') INITIALIZING BACKUP CONTAINER"
+while :
+do
+    # Source the main script here so that it runs in the current shell. Two
+    # important reasons:
+    #
+    # 1. all environment variables are passed in
+    # 2. ensure multiple copies can't be running (as there is no copy)
+    ( . ./backup.sh)
+    sleep 300
+done
