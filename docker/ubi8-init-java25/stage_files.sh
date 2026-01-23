ba3543a50d4ba6440297aa39dde65a166761b76c
JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>
2026-01-23 14:08:03 -0600

    feat: java 25 image (#137)
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

---
+++ docker/ubi8-init-java25/stage_files.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+########################################################################################################################
+# Copyright 2016-2025 Hedera Hashgraph, LLC                                                                            #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#     http://www.apache.org/licenses/LICENSE-2.0                                                                       #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+########################################################################################################################
+
+set -eo pipefail
+files=$(ls -1 /etc/network-node/config)
+for file in $files; do
+  if [ -f "/opt/hgcapp/services-hedera/HapiApp2.0/${file}" ]; then
+    rm -f "/opt/hgcapp/services-hedera/HapiApp2.0/${file}"
+  fi
+  ln -s "/etc/network-node/config/${file}" "/opt/hgcapp/services-hedera/HapiApp2.0/${file}"
+done
+
+# copy hedera.crt and hedera.key to /opt/hgcapp/services-hedera/HapiApp2.0/ if /shared-hapiapp exists and is not empty
+if [ -d "/shared-hapiapp" ] && [ "$(ls -A /shared-hapiapp)" ]; then
+  cp /shared-hapiapp/* /opt/hgcapp/services-hedera/HapiApp2.0/
+fi
+
